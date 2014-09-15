2e60d39578c20c161a0420fff8b15ad49c03bfd2
Victor Felder <victorfelder@gmail.com>
2014-09-15 18:06:57 -0400

    bugfix :D

--- bug/views.py
+++ bug/views.py
@@ -2,7 +2,7 @@ from taggit.models import Tag
 from django.db.models import Count
 
 def root(request):
-    tags = Tag.objects.select_related('links') \
+    tags = Tag.objects.select_related('bug') \
         .annotate(c=Count('bug')).order_by('-c') \
         .all()
     print tags
