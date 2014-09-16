39f726c5d592849d2db8b043a3025eb66c365089
Victor Felder <victorfelder@gmail.com>
2014-09-15 22:11:12 -0400

    Enhance test

--- bug/views.py
+++ bug/views.py
@@ -1,8 +1,17 @@
 from taggit.models import Tag
 from django.db.models import Count
+from bug.models import Bug
+
 
 def root(request):
+    b1, b2 = Bug(), Bug()
+    b1.save()
+    b2.save()
+    b1.tags.add('tag1')
+    b1.tags.add('tag2')
+    b2.tags.add('tag1')
+
     tags = Tag.objects.select_related('bug') \
-        .annotate(c=Count('bug')).order_by('-c') \
+        .annotate(c=Count('bug')).values('c', 'name').order_by('-c') \
         .all()
     print tags
